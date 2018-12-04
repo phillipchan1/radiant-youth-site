c32759a5fc4bcb39f33fb162581c06ebfdcd8269
Phillip Chan <phillip.chan@liferay.com>
2018-12-04 09:00:27 -0800

    Begin retreat work

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,16 +1,15 @@
-import { Component, OnInit, Input} from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
-	selector: 'app-header',
-	templateUrl: './header.component.html',
-	styleUrls: ['./header.component.scss']
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-	@Input() headerLinks;
+  @Input() headerLinks;
+  @Input() theme;
 
-	constructor() { }
+  constructor() {}
 
-	ngOnInit() {
-	}
-
+  ngOnInit() {}
 }
--- src/app/pages/retreat/retreat.component.ts
+++ src/app/pages/retreat/retreat.component.ts
@@ -2,69 +2,72 @@ import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 
 @Component({
-	selector: 'app-retreat',
-	templateUrl: './retreat.component.html',
-	styleUrls: ['./retreat.component.scss']
+  selector: 'app-retreat',
+  templateUrl: './retreat.component.html',
+  styleUrls: ['./retreat.component.scss']
 })
 export class RetreatComponent implements OnInit {
-	starsLoopUrl: string = 'assets/videos/Simple Stars - HD Video Background Loop (1).mp4';
-	intoNightLogoUrl: string = 'assets/images/retreat/retreat-logo-light.png';
-	heroTitle: string = 'Registration is closed! See you guys at retreat!!';
-	heroText: string = '"...and they will shine like stars in the universe..."';
-	retreatVideoUrl: string = 'https://www.youtube.com/embed/tpmpY__38WI';
+  starsLoopUrl: string =
+    'assets/videos/Simple Stars - HD Video Background Loop (1).mp4';
+  intoNightLogoUrl: string = 'assets/images/retreat/retreat-logo-light.png';
+  heroTitle: string = 'Registration is closed! See you guys at retreat!!';
+  heroText: string = '"...and they will shine like stars in the universe..."';
+  retreatVideoUrl: string = 'https://www.youtube.com/embed/tpmpY__38WI';
 
-	retreatNav = [
-		{
-			href: 'about',
-			scroll: true,
-			text: 'About the Retreat'
-		},
-		{
-			href: 'faq',
-			scroll: true,
-			text: 'FAQ'
-		},
-		{
-			href: 'contact',
-			scroll: true,
-			text: 'contact'
-		}
-	];
+  retreatNav = [
+    {
+      href: 'about',
+      scroll: true,
+      text: 'About the Retreat'
+    },
+    {
+      href: 'faq',
+      scroll: true,
+      text: 'FAQ'
+    },
+    {
+      href: 'contact',
+      scroll: true,
+      text: 'contact'
+    }
+  ];
 
-	faqContent = [
-		{
-			question: `Who/What Is Radiant?`,
-			answer: `Radiant Youth is the Youth Ministry of <a href="http://epicentrechurch.org">Epicentre Church</a>, Pasadena, CA`
-		},
-		{
-			question: `What age students are allowed to register?`,
-			answer: `6th-12th graders`
-		},
-		{
-			question: `Will meals be provided?`,
-			answer: `8 meals (Sat, Sun: Breakfast, Lunch, Dinner), (Monday: Breakfast and Lunch). EAT DINNER ON YOUR OWN ON FRIDAY...we have snacks throughout`
-		},
-		{
-			question: `What will accommodations be like?`,
-			answer: `There will be 12 students per same-gender cabin with 2 youth leaders overseeing each cabin`
-		},
-		{
-			question: `How will students be transported?`,
-			answer: `A reputable charter bus company (<a href="https://shofur.com" target="_blank">Shofur</a>) will be taking students to and from the camp.`
-		},
-		{
-			question: `What should we pack?`,
-			answer: `Toiletries, Sleeping Bag/Pillow, Warm Clothes and shoes, Bible, Notebook, Pen, Flashlight`
-		},
-		{
-			question: `What activities will there be?`,
-			answer: `Games, worship, messages, small group times, snow, sports, and more!`
-		}
-	];
+  theme = 'light';
 
-	constructor(private titleService: Title) {
-		titleService.setTitle('Into the Night - Radiant Retreat 2018');
-	}
+  faqContent = [
+    {
+      question: `Who/What Is Radiant?`,
+      answer: `Radiant Youth is the Youth Ministry of <a href="http://epicentrechurch.org">Epicentre Church</a>, Pasadena, CA`
+    },
+    {
+      question: `What age students are allowed to register?`,
+      answer: `6th-12th graders`
+    },
+    {
+      question: `Will meals be provided?`,
+      answer: `8 meals (Sat, Sun: Breakfast, Lunch, Dinner), (Monday: Breakfast and Lunch). EAT DINNER ON YOUR OWN ON FRIDAY...we have snacks throughout`
+    },
+    {
+      question: `What will accommodations be like?`,
+      answer: `There will be 12 students per same-gender cabin with 2 youth leaders overseeing each cabin`
+    },
+    {
+      question: `How will students be transported?`,
+      answer: `A reputable charter bus company (<a href="https://www.ridemcoach.com/" target="_blank">M COach</a>) will be taking students to and from the camp.`
+    },
+    {
+      question: `What should we pack?`,
+      answer: `Toiletries, Sleeping Bag/Pillow, Warm Clothes and shoes, Bible, Notebook, Pen, Flashlight`
+    },
+    {
+      question: `What activities will there be?`,
+      answer: `Games, worship, messages, small group times, snow, sports, and more!`
+    }
+  ];
 
-	ngOnInit() {}
+  constructor(private titleService: Title) {
+    titleService.setTitle('Into the Night - Radiant Retreat 2018');
+  }
+
+  ngOnInit() {}
 }
