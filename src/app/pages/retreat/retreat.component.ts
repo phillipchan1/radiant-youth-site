85aedd25e53bd100e9e3b4abb7f80dfbd84bd259
Phil Chan <phillipchan1@gmail.com>
2019-03-05 09:39:49 -0800

    fix missing dependency

--- src/app/pages/retreat/retreat.component.ts
+++ src/app/pages/retreat/retreat.component.ts
@@ -1,5 +1,5 @@
 import { Title } from '@angular/platform-browser'
-import { Component } from '@angular/core'
+import { Component, OnInit } from '@angular/core'
 
 @Component({
     selector: 'app-retreat',
